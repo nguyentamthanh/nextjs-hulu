7fa17b0a4d75505f6e350dd3c54fd241ff44d7c7
nguyentamthanh <thanhh16012k@gmail.com>
2022-10-21 17:51:36 +0700

    update

---
+++ components/Results.js
@@ -0,0 +1,10 @@
+import FlipMove from "react-flip-move";
+
+function Results({ results }) {
+  return (
+    <FlipMove>
+      <div className="px-5 my-10 sm:grid md:grid-cols-2"></div>
+    </FlipMove>
+  );
+}
+export default Results;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
 
-export default function Home() {
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
+
+export default function Home({ results }) {
   return (
     <div>
       <h1 className="">
@@ -11,6 +28,8 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header></Header>
+        <Nav></Nav>
+        <Results results={results}></Results>
       </h1>
     </div>
   );
